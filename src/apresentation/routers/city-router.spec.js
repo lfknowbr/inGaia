b1989148dd31226a62419b7f52b4f354911b9ce4
Luis Fernando <luis.goncalves@beblue.com.br>
2020-01-07 18:49:08 -0200

    Retorna 500 se o httpRequest for invalido

--- src/apresentation/routers/city-router.spec.js
+++ src/apresentation/routers/city-router.spec.js
@@ -1,6 +1,13 @@
 class CityRouter {
   route (httpRequest) {
-    if (!httpRequest.body.city) {
+    if (!httpRequest || !httpRequest.body) {
+      return {
+        statusCode: 500
+      }
+    }
+
+    const { city } = httpRequest.body
+    if (!city) {
       return {
         statusCode: 400
       }
@@ -12,10 +19,23 @@ describe('City router', () => {
   test('Retorna erro 400 se o nome da cidade não foi informado ', () => {
     const sut = new CityRouter()
     const httpRequest = {
-      body: {}
+      body: {
+      }
     }
     const httpResponse = sut.route(httpRequest)
     console.log(httpResponse)
     expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('Retorna erro 500 se não recebermos um httpRequest ', () => {
+    const sut = new CityRouter()
+    const httpResponse = sut.route()
+    expect(httpResponse.statusCode).toBe(500)
+  })
+
+  test('Retorna erro 500 se o httpRequest não tem body', () => {
+    const sut = new CityRouter()
+    const httpResponse = sut.route({})
+    expect(httpResponse.statusCode).toBe(500)
   })
 })
